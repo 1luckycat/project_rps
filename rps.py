35f3b2014861b9427df59c8423a946fd41159792
1luckycat <027.mott@gmail.com>
2024-01-12 10:43:42 -0600

    first commit

---
+++ rps.py
@@ -0,0 +1,16 @@
+# Project Rock, Paper, and Scissors 
+# Team members: Liz and Mo
+
+
+
+# If player and computer choose the same decision then display ("Game Tied"), 
+
+# If the player chooses "Rock" and the computer chooses "Paper" display ("You lose"), 
+# If the player chooses "Scissors" and the computer chooses "Rock" display ("You Lose")  and 
+# If the player chooses "Paper" and the computer chooses "Scissors" then display ("You lose") 
+# -- Vice versa for other decisions.
+
+# Continue to ask the player for their input until they say "I quit", at which time the game will 
+# then end and display ("Thank you for playing").
+
+
