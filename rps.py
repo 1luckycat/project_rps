25a1a6512ba4f41733f22773f50589a1fd191aab
IzzyVIII <eagb08@gmail.com>
2024-01-15 20:43:32 -0600

    Secondary option for project

--- rps.py
+++ rps.py
@@ -13,4 +13,40 @@
 # Continue to ask the player for their input until they say "I quit", at which time the game will 
 # then end and display ("Thank you for playing").
 
+# import random
+
+# while True:
+#     move = ["rock", "paper", "scissors"]
+#     player = input("Please select (rock, paper, scissors):")
+#     computer = random.choice(move)
+    
+#     print(f" player picked {player}, computer picked {computer}")
+
+#     if player == computer:
+#         print("It's a tie!")
+#     elif player == "rock":
+#         if computer == "paper":
+#             print("You loose! Better luck next time!")
+#     elif player == "scissors":
+#         if computer == "rock":
+#             print("You loose! Better luck next time")
+#     elif player == "paper":
+#         if computer == "scissors":
+#             print("You loose! Better luck next time!")
+#     elif player == "paper":
+#         if computer == "rock":
+#             print("You Win! Way to go!")
+#     elif player == "rock":
+#         if computer == "scissors":
+#             print("You Win! Way to go!")
+#     elif player == "scissors":
+#         if computer== "paper":
+#             print("You Win!Way to go!")
+           
+#     rematch = input("Would you like a rematch? (Type: yes or no)")
+#     if rematch.lower() == "yes":
+#         continue
+#     else:
+#         print("Good game, thanks for playing")
+#         break
 
